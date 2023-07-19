b09f2b19fbb946b64b54d41ae789304b89ca15eb
satish1348 <114284127+satish1348@users.noreply.github.com>
2023-07-18 22:41:18 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function horse(){
+    document.getElementById("animals").src="https://th.bing.com/th?id=OIP.WOHM97Y5e3K2dWLhE6_uSAHaFj&w=288&h=216&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2";
+
+}
+function lion(){
+    document.getElementById("animals").src="https://th.bing.com/th/id/R.1f5f3ebf727ad1ae9e4b8215b273ef4f?rik=57EWyu1Zf%2fJqPA&riu=http%3a%2f%2f4.bp.blogspot.com%2f-xJxeP-SiSSQ%2fUWk0iUK-SUI%2fAAAAAAAAAFs%2fXeF6ccjodcQ%2fs1600%2fLion-King-Of-Jungle.jpg&ehk=R4i82fsv64%2bOyHDIkfdHUDeAR5Q6JhG28yOql%2bAMnrM%3d&risl=&pid=ImgRaw&r=0";
+
+}
+function tiger(){
+    document.getElementById("animals").src="https://th.bing.com/th/id/R.577dcf0d3b8e34a36860381bcc1dac05?rik=HiP3b%2fKbRrBYNA&riu=http%3a%2f%2fs28164.pcdn.co%2ffiles%2fMalayan-Tiger-0155-2199.jpg&ehk=UvueYlfoUwG1xOHcnyY6J2WUWpeQqqF%2f8n4lkhmUU2s%3d&risl=&pid=ImgRaw&r=0";
+
+}
